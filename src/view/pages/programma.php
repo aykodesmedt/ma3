9dee7830170d513cd731ae1f24e1a3287f30f6ef
Ayko De Smedt <ayko.de.smedt@student.howest.be>
2019-01-22 21:13:12 +0100

    styling programma filter

--- src/view/pages/programma.php
+++ src/view/pages/programma.php
@@ -1,43 +1,53 @@
-<form action="">
-  <div>
-    <label for="vrijdag">Vrijdag</label>
-    <input type="radio" id="vrijdag" />
-    <label for="zaterdag">Zaterdag</label>
-    <input type="radio" id="zaterdag" />
-    <label for="zondag">Zondag</label> <input type="radio" id="zondag" />
-  </div>
-  <div>
-    <h3>Soort act:</h3>
-    <label for="alleacts">Alle acts</label>
-    <input type="radio" id="alleacts" />
-    <label for="voorstellingen">Voorstellingen</label>
-    <input type="radio" id="voorstellingen" />
-    <label for="straatacts">Straatacts</label>
-    <input type="radio" id="straatacts" />
-  </div>
+<form class="form">
+    <div class="switch-field switch-dagen">
+      <!-- <div class="switch-title">Three fields? Sure.</div> -->
+      <input type="radio" id="switch_2_left" name="switch_2" value="yes" checked/>
+      <label for="switch_2_left">Vrijdag 24 augustus</label>
+      <input type="radio" id="switch_2_center" name="switch_2" value="maybe" />
+      <label for="switch_2_center">Zaterdag 25 augustus</label>
+			<input type="radio" id="switch_2_right" name="switch_2" value="no" />
+      <label for="switch_2_right">Zondag augustus</label>
+    </div>
+    <div class="switch-field switch-soorten">
+      <div class="switch-title">Soort:</div>
+      <input type="radio" id="switch_3_left" name="switch_3" value="yes" checked/>
+      <label for="switch_3_left">Alle acts</label>
+      <div>
+        <input type="radio" id="switch_3_center" name="switch_3" value="maybe" />
+        <label for="switch_3_center">Voorstellingen</label>
+        <input type="radio" id="switch_3_right" name="switch_3" value="no" />
+        <label for="switch_3_right">Straatacts</label>
+      </div>
+    </div>
 </form>
-<article>
+<section class="slecht_weer">
   <img src="" alt="" />
   <p>
     Bij slecht weer zal er een alternatief programma opgesteld worden dat
     door zal gaan in de gemeentelijke sporthal.
   </p>
-</article>
-<section>
-  <h2>Resultaten</h2>
+</section>
+<section class="resultaten_section">
+  <h2 class="title_underline">Resultaten</h2>
   <ul>
     <?php
       // foreach($acts as $act){
         ?>
-          <article class="resultaten">
-            <img src="" alt="" />
-            <p>TAG</p>
-            <h3>The Sky Is The Limit</h3>
-            <p>by The Flying Dutchmen</p>
-            <p>13.45 - 15. 45</p>
-            <p>13.45 - 15. 45</p>
-            <a href="">MEER WETEN</a>
-          </article>
+          <li>
+            <article class="resultaat">
+              <div class="resultaat_foto">
+                <img src="" alt="" />
+                <span class="tag">Acrobatie</span>
+              </div>
+              <div class="resultaat_info">
+                <h3 class="resultaat_info_act">The Sky Is The Limit</h3>
+                <p class="resultaat_info_group">by The Flying Dutchmen</p>
+                <p class="resultaat_info_hour">13.45 - 15. 45</p>
+                <p class="resultaat_info_hour">13.45 - 15. 45</p>
+                <a href="" class="meerWeten">MEER WETEN</a>
+              </div>
+            </article>
+          </li>
         <?php
       // }
     ?>
