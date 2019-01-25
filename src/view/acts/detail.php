427f1710bf5ee2225a54c8c6cd2ef1ad168a72fc
Ayko De Smedt <ayko.de.smedt@student.howest.be>
2019-01-26 00:49:23 +0100

    detailpagina styling gedaan, online gezet

--- src/view/acts/detail.php
+++ src/view/acts/detail.php
@@ -1,48 +1,78 @@
 <?php
-  // var_dump($act[0]);
+  // var_dump($act);
   // var_dump($_GET['dag']);
   // var_dump($act[0]['id']);
 ?>
+<a href="index.php?page=programma">Terug naar overzicht</a>
 <section>
-  <a href="index.php?page=programma">Terug naar overzicht</a> <img src="" alt="" />
-  <h2><?php echo $act[0]['name_act']; ?></h2>
-  <h3><?php echo $act[0]['name_group']; ?></h3>
-  <span class="tag"><?php echo $act[0]['soort_act']; ?></span>
-  <span class="tag"><?php echo $act[0]['soort_act']; ?></span>
-  <ul>
-    <li>
-      <img src="" alt="icon" />
-      <p><?php echo $act[0]['dag']; ?></p>
-    </li>
-    <li>
-      <img src="" alt="icon" />
-      <p><?php echo $act[0]['uur']; ?></p>
-    </li>
-    <li>
-      <img src="" alt="icon" />
-      <p><?php echo $act[0]['locatie']; ?></p>
-    </li>
-    <li>
-      <img src="" alt="icon" />
-      <p><?php echo $act[0]['website']; ?></p>
-    </li>
-    <li>
-      <img src="" alt="icon" />
-      <p><?php echo $act[0]['land']; ?></p>
-    </li>
-  </ul>
-  <div>
-    <img src="" alt="" />
-    <h4>Over <?php echo $act[0]['name_group']; ?></h4>
-    <p>
-    <?php echo $act[0]['beschrijving']; ?>
-    </p>
+  <div class="detail__div__grid">
+    <div class="detail__div__foto">
+    <img src="./assets/img/acts/jpg/<?php echo $act[0]['img']; ?>.jpg" alt="act" />
+    </div>
+    <div class="detail__div__titels">
+      <div>
+        <h2 class="detail__name_act"><?php echo $act[0]['name_act']; ?></h2>
+        <h3 class="detail__name_group"><?php echo $act[0]['name_group']; ?></h3>
+        <div>
+          <span class="tag"><?php echo $act[0]['soort_act']; ?></span>
+          <span class="tag"><?php echo $act[0]['soort_act']; ?></span>
+        </div>
+      </div>
+    </div>
+    <div class="detail__div__actinfo">
+      <?php
+        foreach($act as $actje):
+      ?>
+        <div class="detail_list_icon">
+          <ul>
+            <li class="detail_listitem_icon">
+              <img src="./assets/img/icons/icon_date.png" alt="icon" />
+              <p><?php echo $actje['dag']; ?></p>
+            </li>
+            <li class="detail_listitem_icon">
+              <img src="./assets/img/icons/icon_time.png" alt="icon" />
+              <p><?php echo $actje['uur']; ?></p>
+            </li>
+            <li class="detail_listitem_icon">
+              <img src="./assets/img/icons/icon_location.png" alt="icon" />
+              <p><?php echo $actje['locatie']; ?></p>
+            </li>
+            <li class="detail_listitem_icon">
+              <img src="./assets/img/icons/icon_website.png" alt="icon" />
+              <p><?php echo $actje['website']; ?></p>
+            </li>
+            <li class="detail_listitem_icon">
+              <img src="./assets/img/icons/icon_world.png" alt="icon" />
+              <p><?php echo $actje['land']; ?></p>
+            </li>
+          </ul>
+        </div>
+      <?php
+        endforeach;
+      ?>
+    </div>
+    <div class="detail__beschrijving__div">
+      <img src="./assets/img/detail_beschrijving_1440.png" alt="" />
+      <div>
+        <h4 class="detail__titel_beschrijving_act">Over <?php echo $act[0]['name_group']; ?></h4>
+        <p class="detail__beschrijving_act">
+        <?php echo $act[0]['beschrijving']; ?>
+        </p>
+      </div>
+    </div>
   </div>
-  <div>
-    <img src="" alt="" /> <img src="" alt="" /> <img src="" alt="" />
-    <img src="" alt="" /> <img src="" alt="" /> <img src="" alt="" />
+
+  <div class="section__ookvandaag">
+    <img src="./assets/img/acts/jpg/maschara.jpg" alt="" />
+    <img src="./assets/img/acts/jpg/maschara.jpg" alt="" />
+    <img src="./assets/img/acts/jpg/maschara.jpg" alt="" />
+    <img src="./assets/img/acts/jpg/maschara.jpg" alt="" />
+    <img src="./assets/img/acts/jpg/maschara.jpg" alt="" />
+    <img src="./assets/img/acts/jpg/maschara.jpg" alt="" />
   </div>
-  <?php echo $act[0]['video']; ?>
+  <div class="video__container">
+    <?php echo $act[0]['video']; ?>
+  </div>
 </section>
 <section class="ookvandaagtitel">
   <h2 class="title_underline">Ook vandaag</h2>
